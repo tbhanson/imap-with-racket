44040dd50e0e9145880039c771d1ad7fd0aefe0c
Tim Hanson <tbh391@gmail.com>
2020-04-05 18:35:06 +0200

    continue refactoring

--- imap-email-account-credentials.rkt
+++ imap-email-account-credentials.rkt
@@ -1,16 +1,22 @@
 #lang racket
 
+; a structure to hold the basics we need
+; these lea
 (struct imap-email-account-credentials
   (accountname hostname mailaddress password)
   #:prefab
   )
 
-(define (read-email-account-credentials port)
+; we chose a serialization of the above such that all we need is read :)
+; cf. https://docs.racket-lang.org/guide/serialization.html#%28tech._serialization%29
+
+(define (read-one-email-account-credential port)
   (read port))
 
-(define (read-email-account-credentials-from-file file)
-  (read-email-account-credentials
-   (open-input-file file)))
+; 
+(define (read-one-email-account-credentials-from-file-named filename)
+  (read-one-email-account-credential
+   (open-input-file filename)))
 
 ; make a container to hold multiple accounts with a way to get one imap-email-account-credentials by accountname for convenience
 (define (read-email-account-credentials-hash-from-port port)
@@ -19,9 +25,17 @@
               ([account account-list])
       (let ((key (imap-email-account-credentials-accountname account)))
         (values (hash-set result key account))))))
-  
+
+(define (read-email-account-credentials-hash-from-file-named filename)
+  (begin
+    ;(assert (string? filename))
+    (printf "(read-email-account-credentials-hash-from-file-named filename (~a)~n" filename)
+    (read-email-account-credentials-hash-from-port
+     (open-input-file filename))))
+   
   
 (provide
+ ; automatic methods from struct
  imap-email-account-credentials
  imap-email-account-credentials?
  
@@ -30,8 +44,10 @@
  imap-email-account-credentials-mailaddress
  imap-email-account-credentials-password
 
- read-email-account-credentials
- read-email-account-credentials-from-file
+ ; reading one or several
+ read-one-email-account-credential
+ read-one-email-account-credentials-from-file-named
  read-email-account-credentials-hash-from-port
+ read-email-account-credentials-hash-from-file-named
  )
  
--- test-imap-email-account-credentials.rkt
+++ test-imap-email-account-credentials.rkt
@@ -3,7 +3,7 @@
 (require rackunit
          "imap-email-account-credentials.rkt")
 
-; test one account, some basics
+; test one account, struct-provided basics
 (let ((some-accountname "my account")
       (some-host "some.host.net")
       (some-address "fred@somewhere.org")
@@ -20,33 +20,50 @@
 ; https://docs.racket-lang.org/guide/serialization.html#%28tech._serialization%29
 ;
 (let ((account-credentials-as-string "#s(imap-email-account-credentials \"an account\" \"ahost.com\" \"me@some.where.net\" \"secret stuff\")"))
-  (let ((test2 (read-email-account-credentials
+  (let ((test2 (read-one-email-account-credential
                 (open-input-string account-credentials-as-string))))
     (check-equal? (imap-email-account-credentials? test2) #t)
-    (check-equal? (imap-email-account-credentials-accountname test2) "an account")))
+    (check-equal? (imap-email-account-credentials-accountname test2) "an account")
+    (check-equal? (imap-email-account-credentials-mailaddress test2) "me@some.where.net")
+    ))
 
 ; test reading one account from a file
 (check-equal?
- (imap-email-account-credentials? (read-email-account-credentials-from-file "test-data/one-test-imap-email-account-credentials.rkt"))
+ (imap-email-account-credentials? (read-one-email-account-credentials-from-file-named "test-data/one-test-imap-email-account-credentials.rkt"))
  #t)
 
-(define filename-containing-two-accounts "test-data/list-of-test-imap-email-account-credentials.rkt")
+(let ([filename-containing-two-accounts "test-data/list-of-test-imap-email-account-credentials.rkt"])
+  (begin
+    (check-equal?
+     (string? filename-containing-two-accounts)
+     #t)
 
-; test reading a list of two accounts from another file
-(check-equal?
- (map imap-email-account-credentials? (read (open-input-file filename-containing-two-accounts)))
- '(#t #t))
+    ; test reading a list of two accounts from another file
+    (check-equal?
+     (map
+      imap-email-account-credentials?
+      (read (open-input-string (file->string filename-containing-two-accounts))))
 
+     '(#t #t))
 
-; test reading a list of two accounts into a hash of accounts
-(check-equal?
- (hash-has-key?
-  (read-email-account-credentials-hash-from-port filename-containing-two-accounts)
-  "an account")
- #t)
+    (check-equal?
+     (string? filename-containing-two-accounts)
+     #t)
 
-(check-equal?
- (hash-has-key?
-  (read-email-account-credentials-hash-from-port filename-containing-two-accounts)
-  "foobar")
- #f)
+    ; this works but seems strange: we expected to test read-email-account-credentials-hash-from-file-named
+    ; but this fails here complaining it has a port instead of a string (!?)
+    ; does the string somehow stand for the opened input file used above?
+    (let ([test-cred-hash (read-email-account-credentials-hash-from-port filename-containing-two-accounts)])
+      (begin
+        ; test reading a list of two accounts into a hash of accounts
+        (check-equal?
+         (hash-has-key?
+          test-cred-hash
+          "an account")
+         #t)
+        
+        (check-equal?
+         (hash-has-key?
+          test-cred-hash
+          "foobar")
+         #f)))))
