ae94be5d1457476ace8d9338891c35aad568fb46
Tim Hanson <tbh391@gmail.com>
2020-04-26 17:49:38 +0200

    continue working on dates

--- parse-mail-dates.rkt
+++ parse-mail-dates.rkt
@@ -8,26 +8,34 @@
  )
 
 (define (possible-parse-date-time-string maybe-date-time-string)
-  '())
+  (parse-datetime maybe-date-time-string "yyyy-MM-dd'T'HH:mm:ssxxx"))
 
 
-(parameterize ([current-locale "en"])
-    (list
-     (parse-time "January 24, 1977" "LLLL d, y")
-     (parse-time "2015-03-15T02:02:02-04:00" "yyyy-MM-dd'T'HH:mm:ssxxx")))
-
-
-(parameterize ([current-locale "en"])
-    (displayln (~t (date 1955 11 12) "E, MMMM d, y G"))
-    (displayln (~t (datetime 1955 11 12 2 42 30) "E h:mm a")))
-
-(append
- (list
-  (iso8601->date "1981-04-05")
-  (iso8601->time "13:47:30")
-  (iso8601->datetime "2014-03-20T19:20:09.3045")
-  )
- (parameterize ([current-locale "en"])
-    (list
-     (parse-date "January 24, 1977" "LLLL d, y")
-     (parse-date "2015-03-15T02:02:02-04:00" "yyyy-MM-dd'T'HH:mm:ssxxx"))))
+;(parameterize ([current-locale "en"])
+;    (list
+;     (parse-time "January 24, 1977" "LLLL d, y")
+;     (parse-time "2015-03-15T02:02:02-04:00" "yyyy-MM-dd'T'HH:mm:ssxxx")))
+;
+;
+;(parameterize ([current-locale "en"])
+;    (displayln (~t (date 1955 11 12) "E, MMMM d, y G"))
+;    (displayln (~t (datetime 1955 11 12 2 42 30) "E h:mm a")))
+;
+;(append
+; (list
+;  (iso8601->date "1981-04-05")
+;  (iso8601->time "13:47:30")
+;  (iso8601->datetime "2014-03-20T19:20:09.3045")
+;  )
+; (parameterize ([current-locale "en"])
+;    (list
+;     (parse-date "January 24, 1977" "LLLL d, y")
+;     (parse-date "2015-03-15T02:02:02-04:00" "yyyy-MM-dd'T'HH:mm:ssxxx")))
+; (parameterize ([current-locale "en"])
+;   (list
+;    (parse-datetime "January 24, 1977" "LLLL d, y")
+;    (parse-datetime "2015-03-15T02:02:02-04:00" "yyyy-MM-dd'T'HH:mm:ssxxx")))
+; )
+;
+;(list
+; (datetime? (parse-datetime "2015-03-15T02:02:02-04:00" "yyyy-MM-dd'T'HH:mm:ssxxx")))
--- test-parse-mail-dates.rkt
+++ test-parse-mail-dates.rkt
@@ -3,7 +3,27 @@
 (require rackunit
          "parse-mail-dates.rkt")
 
+(require gregor)
+
 
 (check-equal?
- (possible-parse-date-time-string "21 Jun 2015 15:45:40 -0000")
+ (datetime? 
+  (possible-parse-date-time-string "2015-03-15T02:02:02-04:00"))
  #t)
+
+(check-equal?
+ (possible-parse-date-time-string "2015-03-15T02:02:02-04:00")
+ (datetime
+  2015 3 15 2 2 2))
+
+(check-exn
+   exn:fail?
+   (lambda ()
+     (raise (make-exn:fail "Hi there"
+                           (current-continuation-marks)))))
+
+
+(check-exn
+   exn:gregor:parse?
+   (lambda ()
+     (possible-parse-date-time-string "21 Jun 2015 15:45:40 -0000")))
