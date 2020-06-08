17c433c91e43dc2b43ac0f1f5e111e11e18762dd
Tim Hanson <tbh391@gmail.com>
2020-06-08 20:57:50 +0200

    basic definition and 1 test of get-mailbox-digest

--- mailbox-digest.rkt
+++ mailbox-digest.rkt
@@ -1,11 +1,55 @@
 #lang racket
 
 (require
-  "mail-digest.rkt"
+  "imap-email-account-credentials.rkt"
+  "connect-to-imap-account.rkt"
+  net/imap
+  "main-mail-header-parts.rkt"
   )
 
-(provide
- mailbox-digest)
+(struct mailbox-digest
+  (mail-address folder-name uid-validity index-range mail-headers)
+  #:prefab
+  )
 
-(define (mailbox-digest)
-  '())
+; 
+(provide
+ (contract-out
+  ; struct automatics
+  [mailbox-digest (-> string? string? integer? pair? list? mailbox-digest?)]
+  [mailbox-digest? (-> any/c boolean?)]
+  [mailbox-digest-mail-address (-> mailbox-digest? string?)]
+  [mailbox-digest-folder-name (-> mailbox-digest? string?)]
+  [mailbox-digest-uid-validity (-> mailbox-digest? integer?)]
+  [mailbox-digest-index-range (-> mailbox-digest? pair?)]
+  [mailbox-digest-mail-headers (-> mailbox-digest? list?)]
+
+  ;
+  [get-mailbox-digest (-> imap-email-account-credentials? string? pair? mailbox-digest?)]
+
+ ))
+
+; 
+(define (get-mailbox-digest mail-account-credential folder-name item-index-range)
+  (let ([imap-conn
+         (securely-connect-to-imap-account mail-account-credential folder-name)])
+    (let ([msg-count (imap-messages imap-conn)]
+          [uid-validity (imap-uidvalidity imap-conn)]
+          [lo-index (car item-index-range)])
+      (let ([hi-index (min msg-count (cdr item-index-range))])
+        (begin
+          (printf "there are ~a messages in folder ~a of account ~a~n" msg-count folder-name (imap-email-account-credentials-accountname mail-account-credential))
+          (printf "we will look at those indexed ~a to ~a~n" lo-index hi-index)
+          (let ([range-of-message-headers
+                 (map
+                  mail-header->main-mail-header-parts
+                  (imap-get-messages
+                   imap-conn
+                   (stream->list (in-range lo-index (+ hi-index 1)))
+                   '(uid header)))]) ; think about whether we also need flags
+            (mailbox-digest
+             (imap-email-account-credentials-mailaddress mail-account-credential)
+             folder-name
+             uid-validity
+             item-index-range
+             range-of-message-headers)))))))
--- test-mailbox-digest.rkt
+++ test-mailbox-digest.rkt
@@ -8,14 +8,26 @@
 
          "mailbox-digest.rkt"
          "imap-email-account-credentials.rkt"
-         "range-of-messages.rkt"
-         "main-mail-header-parts.rkt"
+;         "main-mail-header-parts.rkt"
          )
 
 
 
 (run-tests
  (test-suite
-  "mailbox-digest"
-  ))
-  
+  "get-mailbox-digest"
+  (let ([iniFilePath (default-ini-filepath)])
+    (let ([creds_hash (read-email-account-credentials-hash-from-file-named iniFilePath)]
+          [folder-name "INBOX"]
+          [test-acct-name "tbhanson gmx"]) ;"tim at w-h"
+      (let ([mail-account-credential (hash-ref creds_hash test-acct-name)]
+            [msg-count-to-examine 10])
+        (let ([msg-index-range (cons 1 msg-count-to-examine)])
+          (let ([under-test
+                 (time
+                  (get-mailbox-digest mail-account-credential folder-name msg-index-range))])
+            ; redundant because of contracts
+            (check-equal?
+             (mailbox-digest? under-test)
+             #t))))))))
+              
